0ca3dcc136c62203473369f0976f8aaa91bcd4ed
Shubham <shubham@coderbhai.com>
2018-07-19 15:25:33 +0530

    added validator for expressions and also added tests for it

--- src/syntax.js
+++ src/syntax.js
@@ -51,6 +51,37 @@ function isFalsy (val) {
   return val === false || undefined === val || val === null
 }
 
+function validateExpression(exp, scope, errors = []) {
+  assert(scope, 'unable to evalExp: scope undefined')
+  var operatorREs = lexical.operators
+  var match;
+  for (var i = 0; i < operatorREs.length; i++) {
+    var operatorRE = operatorREs[i]
+    var expRE = new RegExp(`^(${lexical.quoteBalanced.source})(${operatorRE.source})(${lexical.quoteBalanced.source})$`)
+    if ((match = exp.match(expRE))) {
+      errors.concat(validateExpression(match[1], scope, errors), validateExpression(match[3], scope, errors));
+      return errors;
+    }
+  }
+  if((error = validateValue(exp, scope))) {
+    errors.push(error);
+  }
+  return errors;
+}
+
+function validateValue (str, scope) {
+  str = str && str.trim()
+  if (!str) return undefined
+
+  if (lexical.isLiteral(str)) {
+    return;
+  }
+  if (lexical.isVariable(str) && (scope.get(str) === null || scope.get(str) === undefined)) {
+    return `${str} variable not present`
+  }
+  return `cannot eval '${str}' as value`
+}
+
 module.exports = {
-  evalExp, evalValue, isTruthy, isFalsy
+  evalExp, evalValue, isTruthy, isFalsy, validateExpression, validateValue
 }
