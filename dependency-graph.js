abda19b472a3e46bffdada8444c66b2944da53c1
Anirudh Varma <anirudh@spotdraft.com>
2020-11-17 14:57:29 +0530

    Change the return type of parseAssign

--- dependency-graph.js
+++ dependency-graph.js
@@ -10,6 +10,27 @@ function getTemplates(expression, engine) {
   return templates;
 }
 
+function createDependencyTree(text) {
+  const engine = createEngine();
+  const templates = getTemplates(text, engine);
+  const graph = {};
+  templates.forEach(function (tpl) {
+    parseTemplate(tpl, engine, graph);
+  });
+  return graph;
+}
+
+function parseTemplate(template, engine, graph) {
+  if (template.name === "assign") {
+    const dependencyData = parseAssign(template, engine);
+    dependencyData.dependsOn.forEach(function (dependency) {
+      const affectedVariables = graph[dependency] || [];
+      affectedVariables.push(dependencyData.defined);
+      graph[dependency] = affectedVariables;
+    });
+  }
+}
+
 /**
  * Returns an array where the first item is the variable assigned
  * and the rest of the items are the variables used to define it.
@@ -18,13 +39,25 @@ function getTemplates(expression, engine) {
  *
  * @param {*} assignTemplate : The template object representing an Assign tag
  * @param {*} engine: The liquid engine instance
+ *
+ * @returns
+ * ```
+ * {
+ *  // the variable defined
+ *  defined,
+ *  // the variables that the defined variable depends on (does not contain literals)
+ *  dependsOn
+ * }
+ * ```
  */
 function parseAssign(assignTemplate, engine) {
   const definedVar = assignTemplate.tagImpl.key;
   const valueToken = engine.parser.parseValue(assignTemplate.tagImpl.value);
-  let variables = [definedVar];
+  let variables = [];
   if (valueToken.filters) {
-    variables.push(valueToken.initial);
+    if (!Lexical.isLiteral(valueToken.initial)) {
+      variables.push(valueToken.initial);
+    }
     valueToken.filters.forEach((filter) => {
       const notLiterals = filter.args.filter((arg) => {
         return !Lexical.isLiteral(arg);
@@ -32,11 +65,15 @@ function parseAssign(assignTemplate, engine) {
       variables = [...variables, ...notLiterals];
     });
   }
-  return variables;
+  return {
+    defined: definedVar,
+    dependsOn: variables,
+  };
 }
 
 module.exports = {
   parseAssign,
   getTemplates,
   createEngine,
+  createDependencyTree,
 };
--- test/dependency-graph.js
+++ test/dependency-graph.js
@@ -17,28 +17,40 @@ describe("dependency-graph: assign expression parsing", function () {
   it("Should have one variable that depends on one and variable and a literal", () => {
     const expression = `{% assign membership_fee_private_seats_percentaged_two = membership_fee_private_seats_percentaged_one | divided_by: 100.00 %}`;
     const tmpls = depGraph.getTemplates(expression, engine);
-    const graph = depGraph.parseAssign(tmpls[0], engine);
-    expect(graph.length).to.equal(2);
-    expect(graph[0]).to.equal("membership_fee_private_seats_percentaged_two");
-    expect(graph[1]).to.equal("membership_fee_private_seats_percentaged_one");
+    const variableData = depGraph.parseAssign(tmpls[0], engine);
+    expect(variableData.defined).to.equal(
+      "membership_fee_private_seats_percentaged_two"
+    );
+    expect(variableData.dependsOn.length).to.equal(1);
   });
 
   it("Should have one variable that depends on more than one variable", () => {
     const expression = `{% assign membership_fee_private_seats_percentaged_one = membership_fee_private_seats | append: discounts_private_seats_percentage %}`;
     const tmpls = depGraph.getTemplates(expression, engine);
     const graph = depGraph.parseAssign(tmpls[0], engine);
-    expect(graph.length).to.equal(3);
-    expect(graph[0]).to.equal("membership_fee_private_seats_percentaged_one");
-    expect(graph[1]).to.equal("membership_fee_private_seats");
-    expect(graph[2]).to.equal("discounts_private_seats_percentage");
+    expect(graph.defined).to.equal(
+      "membership_fee_private_seats_percentaged_one"
+    );
+    expect(graph.dependsOn.length).to.equal(2);
+    expect(graph.dependsOn[0]).to.equal("membership_fee_private_seats");
+    expect(graph.dependsOn[1]).to.equal("discounts_private_seats_percentage");
   });
 
   it("Should have one variable that is a re-assignment", () => {
     const expression = `{% assign membership_fee_private_seats_percentaged_one = membership_fee_private_seats %}`;
     const tmpls = depGraph.getTemplates(expression, engine);
     const graph = depGraph.parseAssign(tmpls[0], engine);
-    expect(graph.length).to.equal(2);
-    expect(graph[0]).to.equal("membership_fee_private_seats_percentaged_one");
-    expect(graph[1]).to.equal("membership_fee_private_seats");
+    expect(graph.defined).to.equal(
+      "membership_fee_private_seats_percentaged_one"
+    );
+    expect(graph.dependsOn[0]).to.equal("membership_fee_private_seats");
+  });
+});
+
+describe("dependency-graph: parsing complete templates", function () {
+  it("should have 2 variables in the graph", () => {
+    const expression = `{% assign a = x | times: 3 %} {% assign b = a | divided_by: 3 %}`;
+    const graph = depGraph.createDependencyTree(expression);
+    expect(Object.keys(graph).length).to.equal(2);
   });
 });
