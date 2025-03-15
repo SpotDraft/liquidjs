b5c66692999f5f3c397902b70a910cb3b47f4cd5
deev1207 <vend-deev@spotdraft.com>
2025-03-15 23:21:08 +0530

    Added: dynamic table validation, checks for $$ answer assignment in all outer conditional branches

--- test/validations-test.js
+++ test/validations-test.js
@@ -4,70 +4,189 @@ const expect = chai.expect;
 const Liquid = require("../index.js");
 chai.use(sinonChai);
 
-describe("checks whether valid JSON has been passed to parseAssign statements in liquid expressions", () => {
-  const commonSetup = () => {
-    const engine = Liquid({});
-    return engine;
-  };
-
-  it("should return an empty array if parseAssign is assigned a valid JSON value", () => {
-    const engine = commonSetup();
-    const expression = `
-      {% unless a == "EUR" %}
-        {% parseAssign x4 = "test" %}
-      {% endunless %}
-      {% parseAssign x1 = '{"key": "value"}' %}
-        {% parseAssign x0 = "100" %}
-        {% if a == "USD" %}
-          {% parseAssign x1 = "test" %}
-        {% elsif a == "INR" %}
-          {% parseAssign x2 = "[1, 2, 3]" %}
-        {% else %}
-          {% parseAssign x3 = "true" %}
-        {% endif %}
-      `;
-
-    const validationErrors = engine.checkValidJSON(expression);
-    expect(validationErrors).to.deep.equal([]);
+describe("performs validations on liquidJS expressions", () => {
+  before(() => {
+    engine = Liquid({}); // Initialize once before all tests
   });
 
-  it("should return array of errors if parseAssign is assigned invalid JSON value", () => {
-    const engine = commonSetup();
+  describe("checks whether valid JSON has been passed to parseAssign statements in liquid expressions", () => {
+    it("should return an empty array if parseAssign is assigned a valid JSON value", () => {
+      // const engine = commonSetup();
+      const expression = `
+        {% unless a == "EUR" %}
+          {% parseAssign x4 = "test" %}
+        {% endunless %}
+        {% parseAssign x1 = '{"key": "value"}' %}
+          {% parseAssign x0 = "100" %}
+          {% if a == "USD" %}
+            {% parseAssign x1 = "test" %}
+          {% elsif a == "INR" %}
+            {% parseAssign x2 = "[1, 2, 3]" %}
+          {% else %}
+            {% parseAssign x3 = "true" %}
+          {% endif %}
+        `;
 
-    const expression = `
-        {% parseAssign x0 = 10, 0 %}
-        {% parseAssign y = a | plus: b %}
-        {% if a == "USD" and b == "INR" % }
-          {% parseAssign x1 = {"key": value } %}  <!-- Invalid JSON -->
-        {% elsif a == "INR" %}
-          {% parseAssign x2 = [1, 2,] %}      <!-- Invalid JSON -->
+      const validationErrors = engine.checkValidJSON(expression);
+      expect(validationErrors).to.deep.equal([]);
+    });
+
+    it("should return array of errors if parseAssign is assigned invalid JSON value", () => {
+      const expression = `
+          {% parseAssign x0 = 10, 0 %}
+          {% parseAssign y = a | plus: b %}
+          {% if a == "USD" and b == "INR" % }
+            {% parseAssign x1 = {"key": value } %}  <!-- Invalid JSON -->
+          {% elsif a == "INR" %}
+            {% parseAssign x2 = [1, 2,] %}      <!-- Invalid JSON -->
+          {% else %}
+            {% parseAssign x3 = unquotedString %} <!-- Invalid JSON -->
+          {% endif %}
+        `;
+      const expectedValidationErrors = [
+        {
+          expression: "parseAssign x0 =  10, 0",
+          errorMessage:
+            "Invalid value assigned to parseAssign statement at line 2",
+        },
+        {
+          expression: "parseAssign y =  a | plus: b",
+          errorMessage:
+            "Invalid value assigned to parseAssign statement at line 3",
+        },
+        {
+          expression: "parseAssign x2 =  [1, 2,]",
+          errorMessage:
+            "Invalid value assigned to parseAssign statement at line 7",
+        },
+        {
+          expression: "parseAssign x3 =  unquotedString",
+          errorMessage:
+            "Invalid value assigned to parseAssign statement at line 9",
+        },
+      ];
+      const validationErrors = engine.checkValidJSON(expression);
+      expect(validationErrors).to.deep.equal(expectedValidationErrors);
+    });
+  });
+
+  describe("dynamic table validations : checks for presence of $$answer = something in each branch of conditional statements and returns error if not found in a branch", () => {
+    it("should return an empty array if $$answer = something is found in each outer conditional branch of liquid expression", () => {
+      const expression = `
+      {% if x %}
+        {% if y %}
+          $$answer = test
+        {% elsif z %}
         {% else %}
-          {% parseAssign x3 = unquotedString %} <!-- Invalid JSON -->
+          {% if a %}
+            test
+          {% else %}
+            No answer
+          {% endif %}
         {% endif %}
-      `;
-    const expectedValidationErrors = [
-      {
-        expression: "parseAssign x0 =  10, 0",
-        errorMessage:
-          "Invalid value assigned to parseAssign statement at line 2",
-      },
-      {
-        expression: "parseAssign y =  a | plus: b",
-        errorMessage:
-          "Invalid value assigned to parseAssign statement at line 3",
-      },
-      {
-        expression: "parseAssign x2 =  [1, 2,]",
-        errorMessage:
-          "Invalid value assigned to parseAssign statement at line 7",
-      },
-      {
-        expression: "parseAssign x3 =  unquotedString",
-        errorMessage:
-          "Invalid value assigned to parseAssign statement at line 9",
-      },
-    ];
-    const validationErrors = engine.checkValidJSON(expression);
-    expect(validationErrors).to.deep.equal(expectedValidationErrors);
+      {% elsif a %}
+        {% for item in items %}
+          {% if x %}
+            {% if y %}
+              $$random = test
+            {% elsif z %}
+            {% else %}
+              {% if a %}
+                test
+              {% else %}
+                No answer
+              {% endif %}
+            {% endif %}
+          {% elsif a %}
+            {% if y %}
+              $$answer = test
+            {% elsif z %}
+              test
+            {% else %}
+              {% if a %}
+                test
+              {% else %}
+                No answer
+              {% endif %}
+            {% endif %}
+          {% endif %}
+        {% endfor %}
+      {% elsif b %}
+        {% if c %}
+          $$answer = test
+        {% else %}
+          Something else
+        {% endif %}
+      {% else %}
+       $$answer = test
+      {% endif %}
+    `;
+
+      const expectedValidationErrors = [];
+      const validationErrors = engine.checkValidDynamicTable(expression);
+      expect(validationErrors).to.deep.equal(expectedValidationErrors);
+    });
+
+    it("should return an array of errors if $$answer = something is not found in any outer conditional branch of liquid expression", () => {
+      const expression = `
+      {% if x %}
+        {% if y %}
+          $$answer = test
+        {% elsif z %}
+        {% else %}
+          {% if a %}
+            test
+          {% else %}
+            No answer
+          {% endif %}
+        {% endif %}
+      {% elsif a %}
+        {% for item in items %}
+          {% if x %}
+            {% if y %}
+              $$random = test
+            {% elsif z %}
+            {% else %}
+              {% if a %}
+                test
+              {% else %}
+                No answer
+              {% endif %}
+            {% endif %}
+          {% elsif a %}
+            {% if y %}
+              missing 
+            {% elsif z %}
+              test
+            {% else %}
+              {% if a %}
+                test
+              {% else %}
+                No answer
+              {% endif %}
+            {% endif %}
+          {% endif %}
+        {% endfor %}
+      {% elsif b %}
+        {% if c %}
+          $$answer = test
+        {% else %}
+          Something else
+        {% endif %}
+      {% else %}
+       $$answer = test
+      {% endif %}
+    `;
+
+      const expectedValidationErrors = [
+        {
+          branchCondition: "a",
+          errorMessage:
+            "The above branch doesn't contain $$answer assignment expression",
+        },
+      ];
+      const validationErrors = engine.checkValidDynamicTable(expression);
+      expect(validationErrors).to.deep.equal(expectedValidationErrors);
+    });
   });
 });
--- validations.js
+++ validations.js
@@ -14,7 +14,6 @@ function getTemplates(engine, expression) {
 function validateJSON(engine, tpl, value, validationErrors) {
   try {
     const trimmedValue = value.trim();
-
     if (!Lexical.isLiteral(trimmedValue)) {
       throw new Error("Invalid value assigned to parseAssign statement");
     }
@@ -32,6 +31,9 @@ function validateJSON(engine, tpl, value, validationErrors) {
   }
 }
 
+/**
+ * traveres branches and elseTemplates for if templates
+ */
 function parseIf(ifTemplate, callback) {
   const impl = ifTemplate.tagImpl;
   impl.branches.forEach(function (branch) {
@@ -42,8 +44,8 @@ function parseIf(ifTemplate, callback) {
   }
 }
 
-function parseForOrUnless(forTemplate, callback) {
-  const impl = forTemplate.tagImpl;
+function parseForOrUnless(tpl, callback) {
+  const impl = tpl.tagImpl;
   if (impl.templates) {
     callback(impl.templates);
   }
@@ -74,9 +76,142 @@ function checkValidJSON(engine, expression) {
   }
 
   processTemplates(templates);
+
   return validationErrors;
+}
+
+/**
+ - checks if valid dynamic table by looking for presence of $$answer = something in each branch of conditional statements
+ - adds errors to validation errors array
+ * @param {*} expression - liquid expression
+ */
+function checkValidDynamicTable(engine, expression) {
+  const templates = getTemplates(engine, expression);
+  return processTopLevelConditionalBranches(templates);
+}
+
+/**
+ * - Used for dynamic table validation
+ * - Checks whether every top-level branch contains `$$answer = something`
+ *
+ * @example
+ * if()
+ *    $$answer = adskhjkashd
+ * else if()
+ *    $$answer = adskhjkashd
+ * else
+ *    asdasda
+ *
+ * - Should give an error since `$$answer` is missing in the last `else` block.
+ * - Works for nested variables as well.
+ *
+ * @param {*} templates - AST templates
+ */
+
+function processTopLevelConditionalBranches(templates) {
+  const outermostIfBlockTemplate = templates.find(
+    (tpl) => tpl.type === "tag" && tpl.name === "if"
+  );
+  const combinedBranchesArray = combineBranchesAndElseTemplates(
+    outermostIfBlockTemplate
+  );
+
+  return checkAllBranchesContainTarget(combinedBranchesArray);
+}
+
+/**
+ * @example
+ * ifTemplate - {type: 'tag', token: {…}, name: 'if', tagImpl: {…}}
+ * tagImpl - {branches: Array(2), elseTemplates: Array(3)}
+ * @returns combined array of branches and elseTemplate
+ */
+function combineBranchesAndElseTemplates(ifTemplate) {
+  const branchesArray = [
+    ...(ifTemplate?.tagImpl?.branches ?? []),
+    ...(ifTemplate?.tagImpl?.elseTemplates
+      ? [{ templates: ifTemplate.tagImpl.elseTemplates }]
+      : []),
+  ];
+  return branchesArray;
+}
+
+/**
+ * - checks whether all outer branches contain $$ answer assignment
+ * @param {*} combinedBranchesArray - combined array of branches and elseTemplates
+ * @returns
+ */
+function checkAllBranchesContainTarget(combinedBranchesArray) {
+  const validationErrors = [];
+  combinedBranchesArray.every((branch) => {
+    if (!checkValidDynamicTableBranch(branch)) {
+      validationErrors.push({
+        branchCondition: `${branch.cond}`,
+        errorMessage: `The above branch doesn't contain $$answer assignment expression`,
+      });
+      return false;
+    }
+    return true;
+  });
+  return validationErrors;
+}
+
+/**
+ * - Checks if any of the templates in the branch contain $$answer assignment
+ * - returns true as soon as it finds a match
+ * @param {*} branch - branch object
+ * @example
+ * branch -  {cond: 'x', templates: Array(3)}
+ *
+ */
+function checkValidDynamicTableBranch(branch) {
+  return branch.templates.some((tpl) => checkTemplateContainsTarget(tpl));
+}
+
+/**
+ * - checks if template type is html
+ * - Extracts raw value of template and looks for presence of $$answer in it.
+ * @param {*} template
+ */
+function checkTemplateContainsTarget(template) {
+  const pattern = /\$\$\s*answer\s*=\s*/;
+
+  if (template.type === "html") {
+    const rawHtmlDataArray = template.raw
+      ?.split("/n")
+      .map((line) => line.trim())
+      .filter((line) => line != "");
+    return rawHtmlDataArray.some((str) => pattern.test(str));
+  }
+
+  if (template.type === "tag") {
+    return handleTemplateTags(template);
+  }
+}
+
+/**
+ * - handles if, for and unless blocks and checks whether any of them have $$ answer
+ */
+function handleTemplateTags(tpl) {
+  const handledTemplateTypes = ["if", "for", "unless"];
+  if (!handledTemplateTypes.includes(tpl.name)) return;
+
+  if (tpl.name === "if") {
+    const combinedBranchesArray = combineBranchesAndElseTemplates(tpl);
+    return combinedBranchesArray.some((branch) =>
+      checkValidDynamicTableBranch(branch)
+    );
+  }
+
+  if (["for", "unless"].includes(tpl.name)) {
+    return tpl.tagImpl?.templates.some((currentTemplate) =>
+      checkTemplateContainsTarget(currentTemplate)
+    );
+  }
+
+  return false;
 }
 
 module.exports = {
   checkValidJSON,
+  checkValidDynamicTable,
 };
